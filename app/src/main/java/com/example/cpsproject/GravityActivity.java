c275e23d4e9aa26a7dca9c961789298e1aea7255
RaminFaryadHaris <faryad_raminharis@yahoo.com>
2021-03-30 12:01:59 +0430

    add sensorManager

--- app/src/main/java/com/example/cpsproject/GravityActivity.java
+++ app/src/main/java/com/example/cpsproject/GravityActivity.java
@@ -3,13 +3,17 @@ package com.example.cpsproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class GravityActivity extends AppCompatActivity {
+public class GravityActivity extends AppCompatActivity implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,41 @@ public class GravityActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = null;
+        if (sensorManager != null) {
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        }
+
+        SensorEventListener gravityDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent != null) {
+                    System.out.println(": X: " + sensorEvent.values[0] + "; Y: " + sensorEvent.values[1] + "; Z: " + sensorEvent.values[2] + ";");
+                    // TODO: 3/30/2021  
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+        if (sensorManager != null) {
+            sensorManager.registerListener(gravityDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            System.out.println("sensorManager is null");
+        }
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
--- app/src/main/java/com/example/cpsproject/GyroscopeActivity.java
+++ app/src/main/java/com/example/cpsproject/GyroscopeActivity.java
@@ -1,5 +1,9 @@
 package com.example.cpsproject;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,7 +14,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-public class GyroscopeActivity extends AppCompatActivity {
+public class GyroscopeActivity extends AppCompatActivity implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +32,41 @@ public class GyroscopeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = null;
+        if (sensorManager != null) {
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        }
+
+        SensorEventListener gyroscopeDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent != null) {
+                    System.out.println(": X: " + sensorEvent.values[0] + "; Y: " + sensorEvent.values[1] + "; Z: " + sensorEvent.values[2] + ";");
+                    // TODO: 3/30/2021
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+        if (sensorManager != null) {
+            sensorManager.registerListener(gyroscopeDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            System.out.println("sensorManager is null");
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
